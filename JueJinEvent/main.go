1be053f07afafd0208878d66ea4d89de468458ac
RedCrazyGhost <49381700+RedCrazyGhost@users.noreply.github.com>
2024-07-11 16:08:50 +0800

    Update main.go

--- JueJinEvent/main.go
+++ JueJinEvent/main.go
@@ -1,21 +1,49 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
+    "bytes"
+    "encoding/json"
+    "fmt"
 	"io"
 	"net/http"
 	"os"
+    "strconv"
 )
 
+type RequestBody struct {
+	FromDate string
+	Days    int64
+	Aid     int64
+}
+
 func main() {
-	body := os.Getenv("body")
+	fromDate:=os.Getenv("FromDate")
 	cookie := os.Getenv("COOKIE")
 	token := os.Getenv("TOKEN")
-
+    days, err := strconv.ParseInt(os.Getenv("Days"), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	aid, err := strconv.ParseInt(os.Getenv("Aid"), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+    body := &RequestBody{
+		FromDate: 	fromDate,
+		Days: days,
+		Aid: aid,
+	}
+	
+	fmt.Println(fromDate,cookie,token,days,aid)
+	
 	client := &http.Client{}
 
-	buffer := bytes.NewBuffer([]byte(body))
+    bodyBytes, err := json.Marshal(body)
+	if err != nil {
+		panic(err)
+	}
+
+    buffer := bytes.NewBuffer(bodyBytes)
 	req, err := http.NewRequest("POST",
 		"https://api.juejin.cn/event_api/v1/event/month_stat",
 		buffer)
