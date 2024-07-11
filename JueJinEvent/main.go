722fc54f8f4a49a35e8579fd10a4e5e6ec4dec3e
RedCrazyGhost <49381700+RedCrazyGhost@users.noreply.github.com>
2024-07-11 16:09:12 +0800

    Update main.go

--- JueJinEvent/main.go
+++ JueJinEvent/main.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-    "bytes"
-    "encoding/json"
-    "fmt"
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
-    "strconv"
+	"strconv"
 )
 
 type RequestBody struct {
 	FromDate string
-	Days    int64
-	Aid     int64
+	Days     int64
+	Aid      int64
 }
 
 func main() {
-	fromDate:=os.Getenv("FromDate")
+	fromDate := os.Getenv("FromDate")
 	cookie := os.Getenv("COOKIE")
 	token := os.Getenv("TOKEN")
-    days, err := strconv.ParseInt(os.Getenv("Days"), 10, 64)
+	days, err := strconv.ParseInt(os.Getenv("Days"), 10, 64)
 	if err != nil {
 		panic(err)
 	}
@@ -28,22 +28,22 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-    body := &RequestBody{
-		FromDate: 	fromDate,
-		Days: days,
-		Aid: aid,
+	body := &RequestBody{
+		FromDate: fromDate,
+		Days:     days,
+		Aid:      aid,
 	}
-	
-	fmt.Println(fromDate,cookie,token,days,aid)
-	
+
+	fmt.Println(fromDate, cookie, token, days, aid)
+
 	client := &http.Client{}
 
-    bodyBytes, err := json.Marshal(body)
+	bodyBytes, err := json.Marshal(body)
 	if err != nil {
 		panic(err)
 	}
 
-    buffer := bytes.NewBuffer(bodyBytes)
+	buffer := bytes.NewBuffer(bodyBytes)
 	req, err := http.NewRequest("POST",
 		"https://api.juejin.cn/event_api/v1/event/month_stat",
 		buffer)
